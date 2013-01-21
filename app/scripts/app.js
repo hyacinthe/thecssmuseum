f6ef84343b84d39b87e270dd7547133503da255e
Philip Nuzhnyi <callmephilip@Philips-MacBook-Air-2.local>
2013-01-21 15:56:24 -0800

    move application code to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,17 @@
-define([], function() {
-  return 'Hello from Yeoman!';
-});
+(function(window,document){
+    define(["jquery"], function($) {
+        return {
+            run : function(){
+                $(document).ready(function(){
+                    $("#down > h3 > a").on("mouseover", function(){
+                        var marginset = $(".tableau").css("margin-right").replace(/[^-\d\.]/g, '');
+                        var posgall = $("#gallery").css("margin-left").replace(/[^-\d\.]/g, '');
+                        var n = posgall -500 - marginset;
+                        $("#gallery").css("margin-left", n);
+                    });
+                });
+
+            }
+        };
+    });
+})(window,document);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,17 +14,10 @@ require.config({
     'json': 'vendor/json2',
     'text': 'vendor/text',
     'models': 'models',
-    'data': '../data' 
+    'data': '../data'
   }
 });
- 
-require(['jquery'], function($) {
-    $(document).ready(function(){
-        $("#down > h3 > a").on("mouseover", function(){
-            var marginset = $(".tableau").css("margin-right").replace(/[^-\d\.]/g, '');
-            var posgall = $("#gallery").css("margin-left").replace(/[^-\d\.]/g, '');
-            var n = posgall -500 - marginset;
-            $("#gallery").css("margin-left", n);
-        });
-    });
+
+require(['app'], function(App) {
+    App.run();
 });
