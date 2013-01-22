e3d0515a3d5d6b17df0e99d3d8a79d02ceff3a63
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-01-22 00:20:15 -0800

    render gallery

--- app/scripts/views/gallery.js
+++ app/scripts/views/gallery.js
@@ -1,19 +1,38 @@
-define(["underscore","backbone","models/painting"],function(_,Backbone,Painting){
+define(["underscore","backbone","models/painting","handlebars","text!templates/gallery.html"],
 
-    var Gallery = Backbone.View.extend({});
+    function(_,Backbone,Painting,Handlebars,galleryTemplate){
 
-    return {
+        var Gallery = Backbone.View.extend({
 
-        Gallery : Gallery,
+            template : Handlebars.compile(galleryTemplate),
 
-        create : function(paintings){
-            if(typeof paintings === 'undefined'){
-                throw new Error("Paintings required");
+            initialize : function(options){
+                var options = options || {};
+                if(typeof options.models === 'undefined'){
+                    throw new Error("models required");
+                }
+            },
+
+            render : function(){
+                $(this.el).html(this.template());
+                return this.el;
             }
 
-            return new Gallery({
-                models: _.map(paintings, function(p){ return new Painting(); })
-            });
-        }
-    };
-});
+        });
+
+        return {
+
+            Gallery : Gallery,
+
+            create : function(paintings){
+                if(typeof paintings === 'undefined'){
+                    throw new Error("Paintings required");
+                }
+
+                return new Gallery({
+                    models: _.map(paintings, function(p){ return new Painting(); })
+                });
+            }
+        };
+    }
+);
