90a75984151a6fa9543cf317b3f633f1894ab328
Philip Nuzhnyi <callmephilip@Philips-MacBook-Air-2.local>
2013-01-21 16:12:06 -0800

    surpress rendering when testing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,22 @@
 (function(window,document){
     define(["jquery"], function($) {
+
+
+        function testing(){
+            return $("#mocha").length !== 0;
+        }
+
         return {
             run : function(){
                 $(document).ready(function(){
-                    $("#down > h3 > a").on("mouseover", function(){
-                        var marginset = $(".tableau").css("margin-right").replace(/[^-\d\.]/g, '');
-                        var posgall = $("#gallery").css("margin-left").replace(/[^-\d\.]/g, '');
-                        var n = posgall -500 - marginset;
-                        $("#gallery").css("margin-left", n);
-                    });
+                    if(!testing()){
+                        $("#down > h3 > a").on("mouseover", function(){
+                            var marginset = $(".tableau").css("margin-right").replace(/[^-\d\.]/g, '');
+                            var posgall = $("#gallery").css("margin-left").replace(/[^-\d\.]/g, '');
+                            var n = posgall -500 - marginset;
+                            $("#gallery").css("margin-left", n);
+                        });
+                    }
                 });
 
             }
