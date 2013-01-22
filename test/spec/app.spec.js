628b24df07e26d4c2b85b742af9e9006f34cfe88
Philip Nuzhnyi <callmephilip@Philips-MacBook-Air-2.local>
2013-01-21 20:21:04 -0800

    finish application module spec

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,11 +1,15 @@
 define(["app"],function(Application){
 
-	describe('Application', function () {
-	  describe('module', function () {
-	    it('can be imported', function() {
-	      expect(Application).to.be.ok;
-	    });
-	  });
-	});
+    describe('Application', function () {
+        describe('module', function () {
+            it('can be imported', function() {
+                expect(Application).to.be.ok;
+            });
+
+            it('exports run', function(){
+                expect(Application).to.respondTo('run');
+            });
+        });
+    });
 
 });
