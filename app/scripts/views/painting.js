b8f7303bc47abaf2271133c4b859cf18a2241b05
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-22 11:09:40 -0800

    implement rendering for painting view

--- app/scripts/views/painting.js
+++ app/scripts/views/painting.js
@@ -1,12 +1,32 @@
-define(["backbone"],function(Backbone){
+define(["jquery","underscore", "backbone", "handlebars"],function($,_,Backbone,Handlebars){
 
     var Painting = Backbone.View.extend({
 
-    });
+        initialize : function(options){
+            if(typeof this.model === 'undefined'){
+                throw new Error("Model required");
+            }
 
+            require(["text!" + this.model.get("template")], _.bind(function(template){
+                this.template = Handlebars.compile(template); 
+                this.render();
+            },this));
+        },
+
+        render : function(){
+            if(typeof this.template !== 'undefined'){
+                $(this.el).html(this.template());
+            }
+            return this.el;
+        }
+
+    });
 
     return {
         Painting : Painting,
-        create : function(){}
+        
+        create : function(model){
+            return new Painting({ model : model });         
+        }
     };
 });
--- test/spec/views/painting.spec.js
+++ test/spec/views/painting.spec.js
@@ -1,6 +1,17 @@
-define(["views/painting"],function(PaintingView){
+define(["views/painting","models/Painting"],function(PaintingView,Painting){
 
     describe("Views : Painting", function(){
+
+        var template = "templates/gallery/purple/template.html", 
+            painting = new Painting({
+                "template" : template,
+                "name" : "Purple",
+                "author" : "Ellsworth Kelly",
+                "museum" : "National Gallery Of Art",
+                "city" : "Washington D.C.",
+                "country" : "U.S.A.",
+                "year" : 2001
+            });
 
         describe("module", function(){
 
@@ -15,6 +26,50 @@ define(["views/painting"],function(PaintingView){
             it("exports create", function(){
                 expect(PaintingView).to.respondTo("create");
             });
+        });
+
+        describe("create", function(){
+
+            it("requires a model", function(){
+
+                var fn = function(){
+                    PaintingView.create();
+                };
+
+                expect(fn).to.throw("Model required");
+            });
+
+            it("returns a view with model set", function(){
+
+                var v = PaintingView.create(painting);
+
+                expect(v).to.be.ok;
+                expect(v.model).to.equal(painting);
+
+            });
+
+        });
+
+        describe("Painting", function(){
+
+            afterEach(function(){
+                if(typeof require.restore !== 'undefined'){
+                    require.restore();
+                }
+            });
+
+            it("loads template using require", function(done){
+
+                sinon.stub(window,"require",function(deps,callback){
+                    expect(deps.length).to.equal(1);
+                    expect(deps[0]).to.equal("text!"+template);
+                    done();
+                });
+
+                new PaintingView.Painting({ model : painting});
+
+            });
+
         });
 
     });
