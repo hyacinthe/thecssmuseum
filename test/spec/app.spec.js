599b23b6a35a4a016ac6eaf8a7a3f202d529a7a2
Philip Nuzhnyi <callmephilip@Philips-MacBook-Air-2.local>
2013-01-21 20:19:02 -0800

    add application spec

---
+++ test/spec/app.spec.js
@@ -0,0 +1,11 @@
+define(["app"],function(Application){
+
+	describe('Application', function () {
+	  describe('module', function () {
+	    it('can be imported', function() {
+	      expect(Application).to.be.ok;
+	    });
+	  });
+	});
+
+});
